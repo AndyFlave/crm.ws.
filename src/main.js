9e2043be87868abcc2d7c273441fe904d0169407
Andy <infinitumDevelopment@yandex.ru>
2021-06-20 13:24:20 +0300

    added form validation for the register page + firebase connected

--- src/main.js
+++ src/main.js
@@ -4,13 +4,31 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
+import messagePlugin from '@/utils/message.plugin.js'
 import './registerServiceWorker'
-import 'materialize-css/dist/js/materialize.min'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 Vue.config.productionTip = false
 
+Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCB5IQbNOgLc-DJ0nd1pEl4pG2kcvSR55U",
+  authDomain: "vue-crm-cb582.firebaseapp.com",
+  projectId: "vue-crm-cb582",
+  storageBucket: "vue-crm-cb582.appspot.com",
+  messagingSenderId: "105291034588",
+  appId: "1:105291034588:web:a65305de8c3b4655756919",
+  measurementId: "G-WKVTXCT84H"
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
 new Vue({
   router,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -67,6 +67,7 @@
 
 <script>
 import {email, required, minLength} from 'vuelidate/lib/validators'
+import messages from '@/utils/messages'
 
 export default {
   name: 'login',
@@ -81,6 +82,11 @@ export default {
     password: {required, minLength: minLength(12)}
   },
   
+  mounted() {
+    if (messages[this.$route.query.message])
+      this.$message(messages[this.$route.query.message])
+  },
+
   methods: {
     submitHandler() {
       if (this.$v.$invalid) {
